ceec3302554ec8d1cbc02168e03a0f28fec6d4bc
xenophy <kotsutsumi@xenophy.com>
2014-11-25 05:04:44 +0900

    add some specs

--- test/specs/class/Base.js
+++ test/specs/class/Base.js
@@ -318,150 +318,179 @@ describe("CLI.Base", function() {
     });
 
     // }}}
+    // {{{ createAlias
 
-    /*
-    
     describe("createAlias", function() {
+
         var o;
-        
+
         function makeCls(fn, name) {
             var o = {};
             o[name || 'oldFn'] = fn;
             cls = CLI.define(null, o);
         }
-        
+
         afterEach(function() {
             o = null;
         });
-        
+
         it("should create a method on the prototype", function() {
+
             makeCls(function(){});
             cls.createAlias('newFn', 'oldFn');
-            expect(CLI.isFunction(cls.prototype.newFn)).toBe(true);
+
+            assert.equal(CLI.isFunction(cls.prototype.newFn), true);
         });
-        
+
         it("should call through to the old method with the passed arguments", function() {
+
             var a, b;
+
             makeCls(function(arg1, arg2) {
                 a = arg1;
                 b = arg2;
-            });   
+            });
+
             cls.createAlias('newFn', 'oldFn');
+
             o = new cls();
             o.newFn('Val', 17);
-            expect(a).toBe('Val');
-            expect(b).toBe(17);             
+
+            assert.equal(a, 'Val');
+            assert.equal(b, 17);
+
         });
-        
+
         it("should dynamically resolve the old method at runtime", function() {
+
             var values = [];
+
             makeCls(function() {
                 values.push(1);
-            });   
+            });
+
             cls.createAlias('newFn', 'oldFn');
+
             o = new cls();
             o.newFn();
-            
+
             cls.override({
                 oldFn: function() {
                     values.push(2);
                 }
             });
             o.newFn();
-            
+
             o.oldFn = function() {
-                values.push(3);    
+                values.push(3);
             };
+
             o.newFn();
-            expect(values).toEqual([1, 2, 3]);
+
+            assert.deepEqual(values, [1, 2, 3]);
         });
+
     });
-    
+
+    // }}}
+    // {{{ override
+
     describe("override", function() {
+
+        // {{{ mixins
+
         describe("mixins", function() {
+
             var aFn, bFn;
+
             beforeEach(function() {
+
                 aFn = function() {};
                 bFn = function() {};
-                
+
                 CLI.define('spec.Mix1', {
                     extend: 'CLI.Mixin',
                     mixinConfig: {
                         id: 'mix1'
                     },
-                    
+
                     a: aFn
                 });
-                
+
                 CLI.define('spec.Mix2', {
                     extend: 'CLI.Mixin',
                     mixinConfig: {
                         id: 'mix2'
                     },
-                    
+
                     b: bFn
                 });
             });
-            
+
             afterEach(function() {
                 aFn = bFn = null;
                 CLI.undefine('spec.Mix1');
                 CLI.undefine('spec.Mix2');
                 CLI.undefine('spec.MyBase');
             });
-            
+
+            /*
             it("should add mixins on a class without mixins", function() {
+
                 cls = CLI.define('spec.MyBase', {
                 });
-                
+
                 CLI.define(null, {
                     override: 'spec.MyBase',
                     mixins : ['spec.Mix1']
                 });
-                
+
                 expect(cls.prototype.a).toBe(aFn);
                 expect(cls.prototype.mixins.mix1).toBe(spec.Mix1.prototype);
             });
-            
+
             it("should add mixins on a class with existing mixins", function() {
                 cls = CLI.define('spec.MyBase', {
                     mixins: ['spec.Mix1']
                 });
-                
+
                 CLI.define(null, {
                     override: 'spec.MyBase',
                     mixins : ['spec.Mix2']
                 });
-                
+
                 expect(cls.prototype.a).toBe(aFn);
                 expect(cls.prototype.mixins.mix1).toBe(spec.Mix1.prototype);
-                
+
                 expect(cls.prototype.b).toBe(bFn);
                 expect(cls.prototype.mixins.mix2).toBe(spec.Mix2.prototype);
             });
-            
+
             it("should add mixins when the type differs", function() {
                 cls = CLI.define('spec.MyBase', {
                     mixins: {
                         mixFoo: 'spec.Mix1'
                     }
                 });
-                
+
                 CLI.define(null, {
                     override: 'spec.MyBase',
                     mixins : ['spec.Mix2']
                 });
-                
+
                 expect(cls.prototype.a).toBe(aFn);
                 expect(cls.prototype.mixins.mixFoo).toBe(spec.Mix1.prototype);
-                
+
                 expect(cls.prototype.b).toBe(bFn);
                 expect(cls.prototype.mixins.mix2).toBe(spec.Mix2.prototype);
             });
+            */
+
         });
+
     });
-   */
- 
+
+    // }}}
 
 });
 
