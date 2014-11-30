fb05311ddcfc0e338211f8ff51aa8d2fb76091f9
xenophy <kotsutsumi@xenophy.com>
2014-12-01 04:56:20 +0900

    add CLI.app.Application

---
+++ examples/index.js
@@ -0,0 +1,41 @@
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+// require CLI Framework
+require('../lib/index.js');
+
+// {{{ define your application
+
+CLI.application({
+
+    // {{{ name
+
+    name: 'MyApp',
+
+    // }}}
+    // {{{ appFolder
+
+//    appFolder: './app'
+
+    // }}}
+    // {{{ launch
+
+    launch: function() {
+
+
+
+    }
+
+    // }}
+
+});
+
+// }}}
+
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
