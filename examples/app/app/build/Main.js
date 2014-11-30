ba45fb0293e48b9d4989856396b42abecedf3d1e
xenophy <kotsutsumi@xenophy.com>
2014-12-01 06:20:49 +0900

    update

---
+++ examples/app/app/build/Main.js
@@ -0,0 +1,35 @@
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+// {{{ MyApp.app.build.Main
+
+CLI.define('MyApp.app.build.Main', {
+
+    // {{{ extend
+
+    extend: 'CLI.app.Controller',
+
+    // }}}
+    // {{{ run
+
+    run: function() {
+
+        CLI.log([
+            'Execute app build process.'
+        ].join("\n"));
+
+    }
+
+    // }}}
+
+});
+
+// }}}
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
+
